1f67bd475aba76856b281a955a1ed08f6cdcebcd
Luis Gracia <luis.gracia@ebi.ac.uk>
2016-06-07 10:31:28 +0100

    Split cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,10 +1,2 @@
 # Clean packages
 yum -y clean all
-
-# Zero out space
-/sbin/swapoff -a
-/sbin/mkswap /dev/mapper/VG_localhost_sys-LV_swap
-dd if=/dev/zero of=/boot/EMPTY bs=1M
-rm -f /boot/EMPTY
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
---
+++ scripts/zero.sh
@@ -0,0 +1,7 @@
+# Zero out space
+/sbin/swapoff -a
+/sbin/mkswap /dev/mapper/VG_localhost_sys-LV_swap
+dd if=/dev/zero of=/boot/EMPTY bs=1M
+rm -f /boot/EMPTY
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
