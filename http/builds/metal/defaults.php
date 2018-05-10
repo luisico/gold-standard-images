5ee0771d8c4f33a44b7542487ac87c537fd627e8
Luis Gracia <lgraval@gmail.com>
2018-05-10 10:35:30 -0400

    Support network options for metal builds

--- http/builds/metal/defaults.php
+++ http/builds/metal/defaults.php
@@ -2,5 +2,9 @@
 // Metal hardware defaults
 $hardware = "metal";             // Hardware type (virtual/metal)
 $fqdn = "";                      // Full qualified name (metal)
+$device = "";                    // Network device (metal)
 $ipaddr = "";                    // IP address (metal)
+$netmask = "";                   // Netmask (metal)
+$nameserver = "";                // Nameserver (metal)
+$gateway = "";                   // Gateway (metal)
 ?>
--- http/includes/metal/network.php
+++ http/includes/metal/network.php
@@ -1,6 +1,3 @@
 <?php
-$netmask      = '255.255.0.0';
-$nameserver   = '172.16.1.100';
-$gateway      = '172.22.1.1';
-echo "network --bootproto static --ip $ipaddr --hostname $fqdn --netmask $netmask --gateway $gateway --nameserver $nameserver\n";
+echo "network " . ($device != "" ? "--device $device " : "") . "--bootproto static --ip $ipaddr --hostname $fqdn --netmask $netmask --gateway $gateway --nameserver $nameserver\n";
 ?>
