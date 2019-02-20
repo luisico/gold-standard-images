975138105b4bf0280ad60360b88926bce5cfe23f
Luis Gracia <lgraval@gmail.com>
2019-02-20 09:09:02 -0500

    Include HTTP_X_FORWARDED_PREFIX if present when building url
    Support traefik redirections

--- http/includes/metal/bootline.php
+++ http/includes/metal/bootline.php
@@ -1,5 +1,9 @@
 <?php
-$url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://"
+     . $_SERVER['HTTP_HOST']
+     . (isset($_SERVER['HTTP_X_FORWARDED_PREFIX']) ? $_SERVER['HTTP_X_FORWARDED_PREFIX'] : "")
+     . $_SERVER['REQUEST_URI'];
+
 $first_nameserver = explode(',', $nameserver)[0];
 echo "# Boot options:\n";
 echo "# inst.ks=$url selinux=0 bootdev=$device ip=$ipaddr gateway=$gateway netmask=$netmask nameserver=$first_nameserver\n";
