3ec3bd458da8b7d084427cfae281a98cbadbae9b
Luis Gracia <lgraval@gmail.com>
2020-07-09 16:53:21 -0400

    URL for packages has changed in rhel8

--- http/includes/repo.php
+++ http/includes/repo.php
@@ -1,3 +1,7 @@
 <?php
-echo "url --url ${packer_os['repo_server']}/${os_version}/os/x86_64\n";
+if ($os_version_major < 8) {
+  echo "url --url ${packer_os['repo_server']}/${os_version}/os/x86_64\n";
+} else {
+  echo "url --url ${packer_os['repo_server']}/${os_version}/BaseOS/x86_64/os\n";
+}
 ?>
